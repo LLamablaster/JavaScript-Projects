bbb276d796f161ad770b3167ac69ab64e4c0b9ad
Sam Engen <sam.p.engen@gmail.com>
2023-06-09 09:50:50 -0700

    completed initial JS assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+//setting string variables
+var sent1 = "This is the beginning of the string";
+    sent2 = " and this is the end of the string";
+
+//display a window alert
+window.alert(sent1 + sent2);
+//write text to the page
+document.write(3+4);
